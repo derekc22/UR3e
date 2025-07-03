fix end-effector rotation matrix bug (local frame -> global frame), add trajectory generation script, add 3d plotting

mujoco/archive/traj_op3_archive.py
@@ -113,6 +113,7 @@ def ctrl(t, m, d, traj_i):
     
     return np.hstack([
         pos_u + rot_u, 
+        # rot_u, 
         grip_u
     ])
 
@@ -155,7 +156,7 @@ def rot_err(t, m, d, xrot_target):
     
     sensor_site_2f85, xrot_2f85 = get_xrot(m, d, "right_pad1_site") 
     xrot_2f85 = xrot_2f85.reshape(3, 3)
-    # convert xrot_target from body frame-relative to global frame-relative by pre-multiplying by xrot_2f85
+    # convert xrot_target from body frame relative to global frame relative by pre-multiplying by  xrot_2f85
     xrot_target = xrot_2f85 @ xrot_target.reshape(3, 3) 
     
     # #########################################################################################################################
@@ -374,14 +375,15 @@ def main():
     try:
         for t in range (T):
     
-            d.ctrl = ctrl(t, m, d, traj_target[t, :])
+            # d.ctrl = ctrl(t, m, d, traj_target[t, :])
+            ctrl(t, m, d, traj_target[t, :])
 
             mujoco.mj_step(m, d)
             viewer.sync()
             
             traj_true[t] = get_state(m, d)
             
-            print(f"pos_target: {traj_target[t, :3]}, pos_true: {traj_true[t, :3]}, pos_err: {pos_errs[t, :]}")
+            # print(f"pos_target: {traj_target[t, :3]}, pos_true: {traj_true[t, :3]}, pos_err: {pos_errs[t, :]}")
             print(f"rot_target: {R_to_euler(traj_target[t, 3:12].reshape(3, 3))}, rot_true: {R_to_euler(traj_true[t, 3:12].reshape(3, 3))}, rot_err: {R_to_euler(rot_errs[t, :].reshape(3, 3))}")
             print(f"grip_target: {traj_target[t, -1]}")
             print("------------------------------------------------------------------------------------------")
@@ -393,7 +395,7 @@ def main():
     
     finally:
         viewer.close()
-        cleanup()
+        # cleanup()
 
         
         

mujoco/init_mj.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 from utils import *
 np.set_printoptions(
-    linewidth=200,     # Wider output (default is 75)
+    linewidth=400,     # Wider output (default is 75)
     threshold=np.inf,  # Print entire array, no summarization with ...
     precision=8,       # Show more decimal places
     suppress=False     # Do not suppress small floating point numbers
@@ -50,6 +50,9 @@ def main():
     # reset(m, d)
 
     viewer = mujoco.viewer.launch_passive(m, d)
+    viewer.opt.frame = mujoco.mjtFrame.mjFRAME_WORLD
+
+
     # python -m mujoco.viewer --mjcf=./model/ur3e.xml
 
     start = time.time()
@@ -60,8 +63,10 @@ def main():
         viewer.sync()
 
         # print("nq:", m.nq, " nv:", m.nv, " nu:", m.nu)
-        print(d.site("right_pad1_site").xpos.copy())
+        # print(R_to_euler(d.site("right_pad1_site").xmat.reshape(3, 3)))
         
+        # print(d.qpos[0:3])
+
         # print("nq: ", m.nq)
         # print("nv: ", m.nv)
         # print("nu: ", m.nu)

mujoco/plot.py
@@ -1,84 +1,141 @@
-import pickle as pkl
-import glob
-import os
-import matplotlib
-matplotlib.use('Agg')  # Set backend to non-interactive
-import matplotlib.pyplot as plt
 import numpy as np
+from utils import R_to_euler
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # Needed for 3D plotting
 
 
-dirpath = "./logs/pd_keyboard/"
+def plot_trajectory(traj_target, traj_true, pos_errs, rot_errs, T, dtn):
+    
+    t_ = np.arange(T)
+    
+    fig, axes = plt.subplots(3, 3, figsize=(15, 8)) # width, height
+    axes = axes.flatten()
 
-actuators = [
-    "l_hip_yaw_joint",
-    "l_hip_roll_joint",
-    "l_hip_pitch_joint",
-    "l_knee_joint",
-    "l_ankle_joint",
-    "r_hip_yaw_joint",
-    "r_hip_roll_joint",
-    "r_hip_pitch_joint",
-    "r_knee_joint",
-    "r_ankle_joint"
-]
+    # pos plotting
+    pos_2f85_target = traj_target[:, :3]
+    pos_2f85_traj_true = traj_true[:, :3]
+    
+    for i in range(3):
+        ax = axes[i]
+        j = i
+        ax.plot(t_, pos_2f85_target[:, j],'C0', label="target")
+        ax.plot(t_, pos_2f85_traj_true[:, j],'C1', label="true")
+        ax.plot(t_, pos_errs[:, j],'C2', label="err")
+        ax.set_title(["x", "y", "z"][j])
+        ax.grid(True)
+        ax.set_xlim(left=0)
+        # if i == 2:
+        ax.legend(loc='upper right')
+    
+    
+    # convert codebase to jax backend and use jax.vmap instead
+    rot_2f85_target = np.array([R_to_euler(R) for R in traj_target[:, 3:12].reshape(T, 3, 3)])
+    rot_2f85_traj_true = np.array([R_to_euler(R) for R in traj_true[:, 3:12].reshape(T, 3, 3)])
+    
+    for i in range(3, 6):
+        ax = axes[i]
+        j = i-3
+        ax.plot(t_, rot_2f85_target[:, j],'C0', label="target")
+        ax.plot(t_, rot_2f85_traj_true[:, j],'C1', label="true")
+        ax.plot(t_, rot_errs[:, j],'C2', label="err")
+        ax.set_title(["roll", "pitch", "yaw"][j])
+        ax.grid(True)
+        ax.set_xlim(left=0)
+        # if i == 5:
+        ax.legend(loc='upper right')
+        
+
+    ax = axes[6]
+    ax.plot(t_, traj_target[:, 12],'C0', label="target")
+    ax.plot(t_, traj_true[:, 12],'C1', label="true")
+    ax.set_title("grip")
+    ax.grid(True)
+    ax.set_xlim(left=0)
+    ax.legend(loc='upper right')
 
 
-
-
-def generateJointPlots(data_arrs, t_start_idx, data_col_start_idxs, title, ylabels, filename):
-    plt.figure(figsize=(48, 6))
+    mean_pos_errs = np.mean(pos_errs, axis=0)
+    mean_rot_errs = np.mean(rot_errs, axis=0)
+    title = (
+        f"mean_xpos_err:  {mean_pos_errs[0]:.3g}, "
+        f"mean_ypos_err:  {mean_pos_errs[1]:.3g}, "
+        f"mean_zpos_err:  {mean_pos_errs[2]:.3g}, "
+        f"mean_roll_err:  {mean_rot_errs[0]:.3g}, "
+        f"mean_pitch_err: {mean_rot_errs[1]:.3g}, "
+        f"mean_yaw_err:   {mean_rot_errs[2]:.3g}"
+    )
     plt.suptitle(title)
-
-    if len(data_arrs) != len(data_col_start_idxs) != len (ylabels):
-        raise ValueError("data_arrs, data_col_start_idxs, ylabels must all be the same length")
-
-    for i in range(10):  # 10 joints
-        ax1 = plt.subplot(2, 5, i + 1)
-        ax1.plot(timesteps[t_start_idx:], data_arrs[0][t_start_idx:, data_col_start_idxs[0] + i],'C0')
-        ax1.set_ylabel(ylabels[0])
-        ax1.set_title(actuators[i])
-        ax1.tick_params(axis='y', labelcolor='C0')
-        ax1.grid(True)
-
-        if len(data_arrs) > 1:
-            ax2 = ax1.twinx()
-            ax2.plot(timesteps[t_start_idx:], data_arrs[1][t_start_idx:, data_col_start_idxs[1] + i], 'C1')
-            ax2.set_ylabel(ylabels[1])
-            ax2.tick_params(axis='y', labelcolor='C1')
-
-    plt.tight_layout()
-    plt.savefig(os.path.join(logfile_folder, f"plots/{filename}.png"))
-    plt.close()
+    
+    axes[7].set_visible(False)
+    axes[8].set_visible(False)
+    plt.tight_layout()    
+    plt.savefig(f"mujoco/logs/{dtn}/plot.jpg")
+        
 
 
+        
+    
+def plot_3d_trajectory(traj_target, traj_true, pos_errs, dtn):
+    
+    x_target, y_target, z_target = traj_target[:, :3].T
+    x_true, y_true, z_true = traj_true[:, :3].T
+    
+    # Create a figure and 3D axis
+    fig = plt.figure(figsize=(15, 8))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot the line
+    ax.plot(x_target, y_target, z_target, color='C0', label="target")
+    ax.plot(x_true, y_true, z_true, color='C1', label="true")
+
+    # Add labels (optional)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.legend()
+        
+    
+    mean_pos_errs = np.mean(pos_errs, axis=0)
+    title = (
+        f"mean_xpos_err:  {mean_pos_errs[0]:.3g}, "
+        f"mean_ypos_err:  {mean_pos_errs[1]:.3g}, "
+        f"mean_zpos_err:  {mean_pos_errs[2]:.3g}, "
+    )
+    plt.suptitle(title)
+    
+    plt.tight_layout()    
+    plt.savefig(f"mujoco/logs/{dtn}/plot3d.jpg")
+    
 
 
-logfile_folders = glob.glob(os.path.join(dirpath, "*"))
-for logfile_folder in logfile_folders:
-
-    print(f"Generating plots for {logfile_folder}")
-
-    try:
-        with open(os.path.join(logfile_folder, "log.pkl"), 'rb') as logfile:
-            
-            logdata = pkl.load(logfile)
-            os.makedirs(os.path.join(logfile_folder, "plots"), exist_ok=True)
-
-            qpos = np.array(logdata.get("qpos"))
-            qvel = np.array(logdata.get("qvel"))
-            ctrl = np.array(logdata.get("ctrl"))
-
-            simdt = 0.001
-            timesteps = np.arange(stop=len(qpos)*simdt, step=simdt)
-            # Discard initial 2% of data to avoid transients
-            t_start_idx = int(0.02*timesteps.shape[0])            
-
-            # Generate joint plots
-            generateJointPlots([ctrl], t_start_idx, data_col_start_idxs=[0], title="joint_ctrl", ylabels=["u(t) [Nm]"], filename="joint_ctrl")
-            generateJointPlots([qpos], t_start_idx, data_col_start_idxs=[7], title="joint_pos", ylabels=["pos [rad]"], filename="joint_pos")
-            generateJointPlots([qvel], t_start_idx, data_col_start_idxs=[6], title="joint_vel", ylabels=["vel [rad/s]"], filename="joint_vel")
-            generateJointPlots([ctrl, qpos], t_start_idx, data_col_start_idxs=[0, 7], title="joint_ctrl_pos", ylabels=["u(t) [Nm]", "pos [rad]"], filename="joint_ctrl_pos")
 
 
-    except FileNotFoundError:
-        continue
+
+def plot_2d_trajectory(traj_target, traj_true, pos_errs, dtn):
+        
+    fig, axes = plt.subplots(1, 3, figsize=(15, 4)) # width, height
+    axes = axes.flatten()
+
+    indices = [(0, 1), (1, 2), (0, 2)] # (x, y), (y, z), (x, z)
+    
+    for i, j in enumerate(indices):
+        ax = axes[i]
+        ax.plot(traj_target[:, j[0]], traj_target[:, j[1]],'C0', label="target")
+        ax.plot(traj_true[:, j[0]], traj_true[:, j[1]],'C1', label="true")
+        ax.set_title(["xy", "yz", "xz"][i])
+        ax.grid(True)
+        ax.set_xlim(left=0)
+        # if i == 2:
+        ax.legend(loc='upper right')
+
+    
+    mean_pos_errs = np.mean(pos_errs, axis=0)
+    title = (
+        f"mean_xpos_err:  {mean_pos_errs[0]:.3g}, "
+        f"mean_ypos_err:  {mean_pos_errs[1]:.3g}, "
+        f"mean_zpos_err:  {mean_pos_errs[2]:.3g}, "
+    )
+    plt.suptitle(title)
+    
+    plt.tight_layout()    
+    plt.savefig(f"mujoco/logs/{dtn}/plot2d.jpg")

utils.py
@@ -84,6 +84,7 @@ def euler_to_quaternion(euler_angles):
 def euler_to_R(euler_angles):
     
     """Convert Euler angles (radians) to ZYX (yaw-pitch-roll) rotation matrix."""
+    # Note, angles are passed as roll, pitch, yaw, but R is constructued as R = RZ @ RY @ RX (roll first, pitch second, yaw last)
     
     roll, pitch, yaw = euler_angles
     
@@ -112,7 +113,7 @@ def R_to_euler(R):
         pitch = np.pi / 2 if R[2, 0] == -1 else -np.pi / 2
         roll = 0
         yaw = np.arctan2(R[0, 1], R[0, 2]) if R[2, 0] == -1 else np.arctan2(-R[0, 1], -R[0, 2])
-    return np.array([yaw, pitch, roll])  # ZYX order
+    return np.array([roll, pitch, yaw])  # XYZ order
 
 
 
@@ -134,5 +135,5 @@ def get_xrot(m, d, site):
 
 
 def get_grip_ctrl(d):    
-    print(d.sensor("fingers_actuatorfrc").data)
-    return d.sensor("fingers_actuatorfrc").data # actuatorfrc (0, 255)
+    # print(d.sensor("fingers_actuatorfrc").data)
+    return d.sensor("fingers_actuatorfrc").data # actuatorfrc (0, 255) ???